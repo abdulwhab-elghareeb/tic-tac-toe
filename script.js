ef11c02230b77e25cbf141bc7eb59690b8c90263
abdulwhab-elghareeb <abdulwhabelghareeb16@gmail.com>
2026-09-10 16:24:14 +0300

    Display the gameboard

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const GameBoard = (() =>{ 
-    const gameBoard = [["", "", ""],
+const gameBoard = (() =>{ 
+    const gameArray = [["", "", ""],
                        ["", "", ""], 
                        ["", "", ""]]
 
-    const getGameBoard = () => gameBoard;
-    const getCol = (colNumber) => gameBoard.map((row,rowIdx) => gameBoard[rowIdx][colNumber]);
+    const getGameBoard = () => gameArray;
+    const getFlatGameBoard = () => gameArray.flat();
+
+    const getCol = (colNumber) => gameArray.map((row,rowIdx) => gameArray[rowIdx][colNumber]);
     const getAllCols = () => [getCol(0), getCol(1), getCol(2)];
     
-    const markACell = (row, col, playerMark) => {(gameBoard[row][col])? console.log("Invalid Place") : gameBoard[row][col] = playerMark};
+    const markACell = (row, col, playerMark) => (gameArray[row][col])? false : gameArray[row][col] = playerMark;
 
-    return {getGameBoard, markACell, getCol, getAllCols};
+    return {getGameBoard, getFlatGameBoard, getAllCols, markACell, };
 })();
 
 const createPlayer = (playerName, playerMarker) =>{
@@ -19,7 +21,9 @@ const createPlayer = (playerName, playerMarker) =>{
     return {name, marker};
 };
 
-const GameControl = (() => {
+const gameController = (() => {
+    const board = gameBoard.getGameBoard();
+
     let GameIsEnded = false;
 
     const player1 = createPlayer("player1" , "X");
@@ -32,11 +36,11 @@ const GameControl = (() => {
     };
 
     const checkForWinner = () => {
-        const board = GameBoard.getGameBoard();
-        const boardCols = GameBoard.getAllCols();
+        const boardCols = gameBoard.getAllCols();
+        const flatBoard = gameBoard.getFlatGameBoard()
 
-        const numberOfXs = board.flat().filter((cell) => cell == "X").length; // converting the array to 1D and getting the number of "X"s
-        const numberOfOs = board.flat().filter((cell) => cell == "O").length; // the same thing but for "O"s
+        const numberOfXs = flatBoard.filter((cell) => cell == "X").length; // converting the array to 1D and getting the number of "X"s
+        const numberOfOs = flatBoard.filter((cell) => cell == "O").length; // the same thing but for "O"s
 
         // 1- if all the cells of a single row or column are "X" or "O" (horizontal or vertical line)
         // -2 if every cell on the same column as the row number contains "X" or "Y" (i.e., [1][1], [2][2], [3][3])(diagonal line)
@@ -49,7 +53,7 @@ const GameControl = (() => {
                GameIsEnded = true;
                 return (numberOfXs > numberOfOs)? `${player1.name} wins` : `${player2.name} wins`;
 
-           }else if(numberOfOs + numberOfXs === board.flat().length){
+           }else if(numberOfOs + numberOfXs === flatBoard.length){
                 GameIsEnded = true;
                 return "Tie";
            }; 
@@ -58,13 +62,49 @@ const GameControl = (() => {
     const playATurn = (row, col) => {
         if (GameIsEnded) return "The Game Ended"
 
-        GameBoard.markACell(row, col, activePlayer.marker);
-        console.log(GameBoard.getGameBoard());
-
-        switchActivePlayer();
-        console.log(checkForWinner());
-        console.log(`It's now ${activePlayer.name}'s turn`); 
+        if (gameBoard.markACell(+row, +col, activePlayer.marker)){// if it's not an invalid spot
+            switchActivePlayer();
+            checkForWinner();
+        }
     }
 
     return {playATurn};
 })();
+
+const displayController = (() => {
+    board = gameBoard.getGameBoard()
+
+    const renderBoard = (() =>{
+        const gameGrid = document.createElement("div");
+        gameGrid.classList.add("game-grid");
+        
+        board.forEach((row, rowIdx) => board[rowIdx].forEach((col, colIdx) =>{
+            const cellBtn = document.createElement("button")
+            cellBtn.classList.add("cell")
+            cellBtn.setAttribute("data-col", colIdx)
+            cellBtn.setAttribute("data-row", rowIdx)
+            console.log(rowIdx, colIdx)
+
+            gameGrid.appendChild(cellBtn)
+        }))
+
+        document.querySelector("body").appendChild(gameGrid)
+    })()
+
+    const addListeners = (() =>{
+        const cells = document.querySelectorAll(".cell")
+        
+            cells.forEach((cell) => {
+                cell.addEventListener("click", (e) =>{
+                    targetRow = e.target.getAttribute("data-row")
+                    targetCol = e.target.getAttribute("data-col")
+                    
+                    gameController.playATurn(targetRow, targetCol)
+                    cell.textContent = board[targetRow][targetCol]
+                })
+            })
+    })()
+
+    
+})();
+
